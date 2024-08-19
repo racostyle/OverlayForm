d5ee194afaa5d7ab0c0c7f199482c7e36dd2cfca
raco <cekcar@gmail.com>
2024-08-19 16:37:40 +0200

    Initial code

---
+++ OverlayForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ProcessKiller.UiElements
+{
+    partial class OverlayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblProgress = new System.Windows.Forms.Label();
+            pnlProgress = new System.Windows.Forms.Panel();
+            SuspendLayout();
+            // 
+            // lblProgress
+            // 
+            lblProgress.AutoSize = true;
+            lblProgress.Font = new System.Drawing.Font("Arial", 15.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            lblProgress.ForeColor = System.Drawing.SystemColors.InfoText;
+            lblProgress.Location = new System.Drawing.Point(358, 320);
+            lblProgress.Name = "lblProgress";
+            lblProgress.Size = new System.Drawing.Size(87, 30);
+            lblProgress.TabIndex = 0;
+            lblProgress.Text = "label1";
+            // 
+            // pnlProgress
+            // 
+            pnlProgress.BackColor = System.Drawing.SystemColors.HighlightText;
+            pnlProgress.Location = new System.Drawing.Point(263, 115);
+            pnlProgress.Name = "pnlProgress";
+            pnlProgress.Size = new System.Drawing.Size(250, 125);
+            pnlProgress.TabIndex = 1;
+            // 
+            // OverlayForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(pnlProgress);
+            Controls.Add(lblProgress);
+            Name = "OverlayForm";
+            Text = "OverlayForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.Panel pnlProgress;
+    }
+}
---
+++ OverlayForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessKiller.UiElements
+{
+    public partial class OverlayForm : Form
+    {
+        private bool isActive;
+
+        public OverlayForm(Form formToCover)
+        {
+            InitializeComponent();
+
+            this.BackColor = Color.Black;
+            this.Opacity = .85f;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.ControlBox = false;
+            this.TopMost = true;
+            this.ShowInTaskbar = false;
+            this.StartPosition = FormStartPosition.Manual;
+            this.AutoScaleMode = AutoScaleMode.None;
+            this.Location = formToCover.PointToScreen(Point.Empty);
+            this.ClientSize = formToCover.ClientSize;
+            this.FormClosing += OverlayForm_FormClosing;
+
+            var size = this.Width / 9;
+            var posY = this.Height / 2 - size;
+
+            pnlProgress.Width = size;
+            pnlProgress.Height = size;
+            pnlProgress.Location = new Point(size, posY);
+
+            var size2 = size * 1.5f;
+            lblProgress.Text = "Working...";
+            lblProgress.ForeColor = Color.White;
+            lblProgress.Location = new Point(this.Width / 2 - lblProgress.Width / 2, pnlProgress.Location.Y + (int)size2);
+
+            isActive = true;
+            var counter = 0;
+
+            _ = Task.Run(async () =>
+            {
+                while (isActive)
+                {
+                    try
+                    {
+                        switch (counter)
+                        {
+                            case 0:
+                                SetPanelPosition(pnlProgress, size, posY);
+                                break;
+                            case 1:
+                                SetPanelPosition(pnlProgress, size * 3, posY);
+                                break;
+                            case 2:
+                                SetPanelPosition(pnlProgress, size * 5, posY);
+                                break;
+                            default:
+                                SetPanelPosition(pnlProgress, size * 7, posY);
+                                break;
+                        }
+                        counter = counter < 3 ? counter + 1 : 0;
+                    }
+                    catch (Exception ex)
+                    {
+                        await Console.Out.WriteLineAsync(ex.Message);
+                    }
+                    finally
+                    {
+                        await Task.Delay(150);
+                    }
+                }
+            });
+        }
+
+        private void OverlayForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            isActive = false;
+        }
+
+        private void SetPanelPosition(Panel panel, int posX, int posY)
+        {
+            if (panel.InvokeRequired)
+            {
+                panel.BeginInvoke((MethodInvoker)delegate ()
+                {
+                    panel.Location = new Point(posX, posY);
+                });
+            }
+            else
+            {
+                panel.Location = new Point(posX, posY);
+            }
+        }
+    }
+}
